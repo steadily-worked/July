fcb8e85ac9b58c41f107d90641e50349537474fd
sangminpark <qpwpep5429@gmail.com>
2020-07-06 23:54:28 +0900

    7/6 JS 형제간호칭 문제

---
+++ JavaScript/brotherhood_naming.js
@@ -0,0 +1,40 @@
+function whatShouldICallYou(myAge, myGender, yourAge, yourGender) {
+    if (myAge === yourAge) {
+      return "친구"
+    }
+    else if (myAge > yourAge) {
+        if (yourGender === 'male') {
+          return "남동생"
+        }
+        else {
+          return "여동생"
+        }
+      }
+    else if (myAge < yourAge) {
+      if (myGender === 'male') {
+        if (yourGender === 'male') {
+          return "형"
+        }
+        else {
+          return "누나"
+        }
+      }
+      else {
+        if (yourGender === 'male') {
+          return "오빠"
+        }
+        else {
+          return "언니"
+        }
+      }
+    }
+  }
+  
+  // 테스트 코드
+  var result1 = whatShouldICallYou(23, 'male', 25, 'female');
+  var result2 = whatShouldICallYou(21, 'female', 21, 'female');
+  var result3 = whatShouldICallYou(25, 'female', 22, 'male');
+  
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
