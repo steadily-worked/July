c832171655cc7f39aa07748efe38a9d85f97c045
sangminpark <qpwpep5429@gmail.com>
2020-07-13 19:03:26 +0900

    JS 7/13 홀짝 카드 완성본

--- JavaScript/oddAndEvenCard_JavaScript/js/script.js
+++ JavaScript/oddAndEvenCard_JavaScript/js/script.js
@@ -1,16 +1,15 @@
 $('#even-btn').on('click', evenBtnFunction);
 $('#odd-btn').on('click', oddBtnFunction);
 
-function evenBtnFunction()  {
+function evenBtnFunction() {
     $('.card').removeClass('selected');
     $('.card').each(function() {
         var num = $(this).text();
         if (num % 2 == '0') {
             $(this).addClass('selected');
-        }
-    })
-    
-    };
+        };
+    });
+};
 
 function oddBtnFunction() {
     $('.card').removeClass('selected');
@@ -18,6 +17,6 @@ function oddBtnFunction() {
         var num = $(this).text();
         if (num % 2 == '1') {
             $(this).addClass('selected');
-        }
-    })
+        };
+    });
 };
